e2dd06c7a1840f879e46caa73c7e6d404dcc5643
Anbang <79228807@qq.com>
2023-04-13 17:54:29 +0800

    support okc

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,23 +4,33 @@ import { ethers } from 'hardhat'
 console.log('hre.network.name', hre.network.name)
 
 // ************ 配置:开始 ************
-const locationId = 1
+const CONFIG = {
+  fbchain: {
+    locationId: 1,
+  },
+  // hardhat / okchain
+  okchain: {
+    locationId: 2,
+  },
+}
+const config = CONFIG[hre.network.name]
 // ************ 配置:结束 ************
 
 // current network info
 console.log('\n ============ BASIC  INFO ============')
 console.log('✅ Current Network   :', hre.network.name)
+console.log('        locationId   :', config.locationId)
 console.log('   Network Url       :', hre.network.config.url || 'hardhat temp network')
 
 async function main() {
   const [deployer] = await ethers.getSigners()
   console.log('✅ Deployer Address  :', deployer.address)
 
-  console.log('\n ____________ DEPLOY INFO ____________')
+  console.log(`\n ____________ DEPLOY INFO ____________`)
 
   //deploy
   const Social = await ethers.getContractFactory('SocialX')
-  const SocialContract = await Social.deploy(locationId)
+  const SocialContract = await Social.deploy(config.locationId)
   await SocialContract.deployed()
   console.log(`SocialX              : ${SocialContract.address}`)
 
